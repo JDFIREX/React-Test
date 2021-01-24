7e9f163fdb59004f025b51253033769bce565081
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-01-24 22:42:51 +0100

    .

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
     render() {
         return(
-            <div><h1>Hello World!</h1></div>
-            <h2>Test vercel</h2>
+            <div>
+                <h1>Hello World!</h1>
+                <h2>Test</h2>
+            </div>
         )
     }
 }
