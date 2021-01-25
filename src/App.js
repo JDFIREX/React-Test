89ea93c1e3433425cd9309785cbf8cd1bcc9858c
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-01-25 13:53:46 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-class App extends React.Component {
+export class Hello extends React.Component {
     render() {
         return(
             <div>
@@ -12,5 +11,201 @@ class App extends React.Component {
     }
 }
 
+export class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked : false,
+            value : "Not Clicked"
+        };
+        this.ChangeStatus = this.ChangeStatus.bind(this);
+    }
 
-export default App;
+    ChangeStatus() {
+        this.setState(state => ({
+            clicked : !state.clicked,
+            value : !state.clicked ? "Clicked" : "Not Clicked"
+        }))
+    }
+
+    render(){
+        return (
+            <div>
+                <button 
+                    className="btn"
+                    data-clicked={this.state.clicked}
+                    onClick={this.ChangeStatus}>
+                    {this.state.value}
+                </button>
+            </div>
+        )
+    }
+}
+
+class List extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <ul>
+                {
+                    this.props.fetch.map(item => {
+                        return (
+                        <li
+                            className="item"
+                            data-index={item.index}>
+                            {item.mess}
+                        </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
+}
+
+
+
+
+export class CreateList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.fetch = [
+            {
+                index : "0",
+                mess : "wenkjr"
+            },
+            {
+                index : "1",
+                mess : "wenkjr"
+            },
+            {
+                index : "2",
+                mess : "wenkjr"
+            },
+            {
+                index : "3",
+                mess : "wenkjr"
+            },
+            {
+                index : "4",
+                mess : "wenkjr"
+            },
+        ]
+    }
+
+    render(){
+        return (
+            <div>
+                <List fetch={this.fetch}/>
+            </div>
+        )
+    }
+
+}
+
+
+
+function ListItem(props) {
+    // Dobrze! Nie ma potrzeby definiowania klucza tutaj:
+    return <li>{props.value}</li>;
+}
+   
+export function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        // Dobrze! Klucz powinien zostać ustawiony na elementach wewnątrz tablicy.
+        <ListItem key={number.toString()} value={number} />
+    );
+    return (
+        <ul>
+        {listItems}
+        </ul>
+    );
+}
+
+
+export class Clock extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            date : new Date()
+        }
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(() => this.tick(),1000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerID)
+    }
+    tick(){
+        this.setState({
+            date: new Date()
+        })
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.date.toLocaleTimeString()}</h1>
+            </div>
+        )
+    }
+}
+
+
+
+export class Checked extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count : 0,
+            value : ""
+        }
+        this.checked = this.checked.bind(this)
+    }
+
+    checked(e){
+        this.setState(state => ({
+            count : state.count + 1,
+            value : `this ${e.target.outerHTML} - ${state.count}`
+        }))
+    }
+    render(){
+        return(
+            <div>
+                <button onClick={(e) => this.checked(e)}>Kliknij</button>
+                <h1>{this.state.value}</h1>
+            </div>
+        )
+    }
+}
+
+
+export class Login extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogin : false
+        }
+    }
+
+
+
+
+    render(){
+        return(
+            <div>
+                <button
+                onClick={this.}>
+                    {this.state.isLogin ? "Wyloguj się" : "Zaloguj się"}
+                </button>
+            </div>
+        )
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import * as App from "./App";
 
-ReactDOM.render(<App/>, document.querySelector(".root"))
+
+const numbers = [1, 2, 3, 4, 5];
+
+
+
+class Index extends React.Component {
+    render() {
+        return (
+            <>
+                <App.Hello />
+                <hr />
+                <App.Hello />
+                <hr />
+                <App.Button />
+                <hr />
+                <App.CreateList />
+                <hr />
+                <App.NumberList numbers={numbers} />
+                <hr />
+                <App.Clock />
+                <hr />
+                <App.Checked />
+                <hr />
+            </>
+        )
+    }
+}
+
+
+
+ReactDOM.render(<Index />, document.querySelector(".root"))
