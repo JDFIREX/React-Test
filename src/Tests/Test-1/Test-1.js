3a27716531d17122d542d7cdd9739de75a08f6d6
JDFIREX <42634156+JDFIREX@users.noreply.github.com>
2021-02-05 20:28:00 +0100

    .

--- src/Tests/Test-1/Test-1.js
+++ src/Tests/Test-1/Test-1.js
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import "./Test-1.css"
+
+
+
+// 
+//  header
+// 
+
+
+
+function useWindowSize(){
+    const [size, setSize] = useState(0);
+    useLayoutEffect(() => {
+        function updateSize() {
+          setSize(window.innerWidth);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+      }, []);
+      return size;
+}
+
+function Nav(){
+    const width = useWindowSize();
+
+    if(width > 1000){
+        return  (
+            <div>
+                <h1>desktop</h1>
+            </div>
+        )
+    }else{
+        return (
+            <div>
+                <h1>Mobile</h1>
+            </div>
+        )
+    }
+}
+
+
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            hamburger : false
+        }
+    }
+
+    render(){
+        
+        return(
+            <div className="Header" >
+                <h1>React with import css file</h1>
+                <Nav/>
+            </div>
+        )
+    }
+}
+
+
+
+
+// 
+// main app
+// 
+
+
 
 export default class Test1 extends React.Component {
     constructor(props){
@@ -11,7 +79,7 @@ export default class Test1 extends React.Component {
 
         return (
             <>
-                <h1>Hello World Test - 1</h1>
+                <Header />
             </>
         )
     }
